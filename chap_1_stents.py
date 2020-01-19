66ac27abb0752f301bcf9d8b45b9c24fbf4f97cb
ukrainian-serge <plutobot2004@hotmail.com>
2020-01-19 12:58:57 -0600

    CHAP 1. Finding better ways to design the table using pivot_table()

--- chap_1_stents.py
+++ chap_1_stents.py
@@ -1,3 +1,12 @@
+### SOME FUNCS
+def get_funcs(thing):
+  """
+  Prints available functions
+  """
+     for i in dir(thing):
+         if not i.startswith('_'):
+             print(i)
+
 
 ### Chapter 1: introduction to data
 
@@ -34,6 +43,8 @@ grouped_30.merge(grouped_365, left_index=True, right_index=True, suffixes=('_30_
 
 ##############################
 ### I THINK THIS DOES IT!! ###
+import pandas as pd
+import numpy as np
 
 p_30 = pd.read_csv("./stent30.csv", header=0)
 p_365 = pd.read_csv("./stent365.csv", header=0)
@@ -42,16 +53,24 @@ p_365 = pd.read_csv("./stent365.csv", header=0)
 grouped_30 = p_30.groupby(['group', 'outcome']).agg('size').unstack()
 grouped_365 = p_365.groupby(['group', 'outcome']).agg('size').unstack()
 
-column_titles = ['stroke', 'no event']
-grouped_30 = grouped_30[column_titles]
-grouped_365 = grouped_365[column_titles]
-
+## column_titles = ['stroke', 'no event']
+## grouped_30 = grouped_30[column_titles]
+## grouped_365 = grouped_365[column_titles]
 
 concatted = pd.concat([grouped_30, grouped_365], axis=1).sort_index(ascending=False)
-concatted.columns.names = [None, None]
+## concatted.columns.names = [None, None]
 
 ## for multi index columns
 columns = [('0-30_days', 'no_event'),('0-30_days', 'stroke'),('0-365_days', 'no_event'),('0-365_days', 'stroke')]
 concatted.columns = pd.MultiIndex.from_tuples(columns)
 
+###############################################
+### ANOTHER ATTEMPT AND FORMING THE TABLE #####
 
+p_30.pivot_table(index='group', columns='outcome', aggfunc=len)
+p_365.pivot_table(index='group', columns='outcome', aggfunc=len)
+
+
+
+
+oncatted['0-30_days']['stroke']['control'] ### GREAT WAY TO SELECT MULTIINDEX STUFF
