d98834f83a1da3ccb467a8aef2b8e680834a088b
ukrainian-serge <plutobot2004@hotmail.com>
2020-02-19 19:30:04 -0600

    added outlier annotation function to /snippets.py module

--- code_snippets_settings/snippets.py
+++ code_snippets_settings/snippets.py
@@ -9,18 +9,11 @@ def get_funcs(thing):
         if not i.startswith('_'):
             print(i)
 
-# def outliers_df(df, x, y, upper_q=0.0, lower_q=1):
-#     return df[(df[x] > df[x].quantile(upper_q) &
-#                 df[y] < df[y].quantile(lower_q))]
-
-
 
 def plot_ouliers_annotate(df, x, y, alpha=0.7, s=60, c='salmon', offset_text=1, txt_1=None, txt_2=None):
 
     plt.scatter(data=df, x=x, y=y, alpha=alpha, s=s, c=c)
-
     df_range = range(df.shape[0])
-
     x, y = df[x].tolist(), df[y].tolist()
 
     if txt_1: 
